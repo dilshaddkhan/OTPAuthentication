c955859b6ac0399e7030a56941d0ed3013bd617c
dilshad <dilshad.khan@agranee.com>
2018-09-25 16:56:33 +0530

    first commit

---
+++ app/src/main/java/com/example/dilshad/dummyotpapp/MainActivity.java
@@ -0,0 +1,196 @@
+package com.example.dilshad.dummyotpapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.READ_SMS;
+import static android.Manifest.permission.RECEIVE_SMS;
+import static android.Manifest.permission.SEND_SMS;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGenerateOTP, btnSignIn;
+
+    EditText etPhoneNumber, etOTP;
+
+    String phoneNumber, otp;
+
+    FirebaseAuth auth;
+    public static final int RequestPermissionCode = 1;
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback;
+    private String verificationCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        requestPermission();
+        checkPermission();
+        StartFirebaseLogin();
+
+        btnGenerateOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneNumber = etPhoneNumber.getText().toString();
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        phoneNumber,                     // Phone number to verify
+                        60,                           // Timeout duration
+                        TimeUnit.SECONDS,                // Unit of timeout
+                        MainActivity.this,        // Activity (for callback binding)
+                        mCallback);                      // OnVerificationStateChangedCallbacks
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otp = etOTP.getText().toString();
+
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, otp);
+
+                SigninWithPhone(credential);
+            }
+        });
+    }
+
+    private void SigninWithPhone(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(MainActivity.this, SignedIn.class));
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Incorrect OTP", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void findViews() {
+        btnGenerateOTP = findViewById(R.id.btn_generate_otp);
+        btnSignIn = findViewById(R.id.btn_sign_in);
+
+        etPhoneNumber = findViewById(R.id.et_phone_number);
+        etOTP = findViewById(R.id.et_otp);
+    }
+
+    private void StartFirebaseLogin() {
+
+        auth = FirebaseAuth.getInstance();
+        mCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                Toast.makeText(MainActivity.this, "verification completed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(MainActivity.this, "verification fialed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                verificationCode = s;
+                Toast.makeText(MainActivity.this, "Code sent", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equalsIgnoreCase("otp")) {
+                final String message = intent.getStringExtra("message");
+                etOTP.setText(message);
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode, message);
+                SigninWithPhone(credential);
+                //Do whatever you want with the code here
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("otp"));
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                {
+                        INTERNET,
+                        RECEIVE_SMS,
+                        READ_SMS,
+                        SEND_SMS
+                }, RequestPermissionCode);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case RequestPermissionCode:
+
+                if (grantResults.length > 0) {
+                }
+                break;
+        }
+    }
+
+    public boolean checkPermission() {
+
+        int FirstPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), INTERNET);
+        int SecondPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), RECEIVE_SMS);
+        int ThirdPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), READ_SMS);
+        int FourthPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), SEND_SMS);
+
+        return FirstPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                SecondPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                ThirdPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                FourthPermissionResult == PackageManager.PERMISSION_GRANTED ;
+
+    }
+
+}
